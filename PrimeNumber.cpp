53b5cded58bd3b6047cc1f2d6df61ae21d94d5c8
QQCoder <emilmaks86@gmail.com>
2016-02-16 15:21:12 +0300

    Create PrimeNumber.cpp

---
+++ PrimeNumber.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+bool isPrime(int x) {
+	int i;
+	int result = 1;
+	for (i = 2; i*i < x; i++) {
+		if (x % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	int x;
+	printf("Enter some number: ");
+	scanf("%d", &x);
+	if (isPrime(x)) {
+		printf("%d is prime number", x);
+	} else {
+		printf("%d isn't prime number", x);
+	}
+	return 0;
+}
